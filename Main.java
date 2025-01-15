f9ad19230e81239c15478ec57febc793f7a85951
Titoputraaa <titobamulo13@gmail.com>
2025-01-15 23:45:53 +0700

    next

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main { // to do list program with stack 
+
+    public static void main(String[] args) {
+        do {
+            System.out.println("===MENU===");
+            System.out.println("1 : SHOW LIST");
+            System.out.println("2 : ADD LIST ");
+            System.out.println("3 : REMOVE LIST ");
+        } while (true);
+    }
+}
