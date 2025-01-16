f9286787f6ccc612115e8393c1b968ff4d953be5
Titoputraaa <titobamulo13@gmail.com>
2025-01-16 22:52:54 +0700

    make method to created new note

--- Main.java
+++ Main.java
@@ -1,11 +1,51 @@
-public class Main { // to do list program with stack 
+import java.util.Scanner;
+
+public class Main { // NOTE program with stack 
+    static Scanner sc = new Scanner(System.in);
+    static int jml_note = 1;
+    static String[] head_note = new String[jml_note];
+    static String[] content = new String[jml_note];
+    
 
     public static void main(String[] args) {
+        System.out.println("===WELCOME TO NOTE APK===");
         do {
-            System.out.println("===MENU===");
-            System.out.println("1 : SHOW LIST");
-            System.out.println("2 : ADD LIST ");
-            System.out.println("3 : REMOVE LIST ");
+            System.out.println("\n===MENU===");
+            System.out.println("1 : CREATE");
+            System.out.println("2 : UPDATE");
+            System.out.println("3 : READ");
+            System.out.println("4 : DELETE");
+            System.out.print("pick : ");
+            int pick = sc.nextInt();
+
+            switch (pick) {
+                case 1:
+                    create_new();
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+            
+                default:
+                System.out.println("EROR, please pick number between 1 - 4 ");
+                    break;
+            }
         } while (true);
+    }
+
+    static void create_new() {
+        for (int i = 0; i < jml_note; i++) {
+            System.out.print("Note Name : ");
+            head_note[i] = sc.nextLine();
+            sc.nextLine();
+            System.out.println("Contents");
+            content[i] = sc.nextLine();
+        }
     }
 }
