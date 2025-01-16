c5d31d0566af75bcf34bb16b8b9f84887eece1ae
Titoputraaa <titobamulo13@gmail.com>
2025-01-16 23:31:11 +0700

    have'n make method to update and delate

--- Main.java
+++ Main.java
@@ -10,15 +10,16 @@ public class Main { // NOTE program with stack
     public static void main(String[] args) {
         System.out.println("===WELCOME TO NOTE APK===");
         do {
-            System.out.println("\n===MENU===");
+            System.out.println("\n===MENU==="); // menu program so the user can choose what he gaone do 
             System.out.println("1 : CREATE");
             System.out.println("2 : UPDATE");
             System.out.println("3 : READ");
             System.out.println("4 : DELETE");
+            System.out.println("5 : EXIT");
             System.out.print("pick : ");
             int pick = sc.nextInt();
 
-            switch (pick) {
+            switch (pick) { // swich case to execute the user pick by case 
                 case 1:
                     create_new();
                     break;
@@ -31,15 +32,17 @@ public class Main { // NOTE program with stack
                 case 4:
                     
                     break;
-            
+                case 5:
+                    return;   
+                    
                 default:
-                System.out.println("EROR, please pick number between 1 - 4 ");
+                System.out.println("EROR, please pick number between 1 - 5 ");
                     break;
             }
         } while (true);
     }
 
-    static void create_new() {
+    static void create_new() { // method to make new note 
         for (int i = 0; i < jml_note; i++) {
             System.out.print("Note Name : ");
             sc.nextLine();
@@ -47,10 +50,11 @@ public class Main { // NOTE program with stack
             System.out.println("Contents");
             content[i] = sc.nextLine();
         }
+        jml_note++;
     }
 
     // method to show note 
-    static void read() {
+    static void read() { // method to show the note
         for (int i = 0; i < jml_note; i++) {
             System.out.println ("\n" + head_note[i]);
             System.out.println("---------------------------------------");
